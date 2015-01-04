4c226e4437589c8850243e0ee5da2f66613bc196
Matt Ferraro <matt@planet.com>
2015-01-04 00:45:55 -0800

    Added newgame http endpoint which basically just generates a UUID

--- src/http.go
+++ src/http.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"net/http"
 	"text/template"
+	_ "io/ioutil"
+	"io"
 	"github.com/gorilla/mux"
+	"github.com/twinj/uuid"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -28,11 +31,35 @@ func game(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hit game.html")
 }
 
+func newGame(w http.ResponseWriter, r *http.Request) {
+	// The UUID for this game which will be used everywhere
+	// as this game's identifier
+	u := uuid.NewV4()
+	io.WriteString(w, u.String())
+}
+
 func main() {
+	// The default UUID format has gross curly brackets in it!
+	uuid.SwitchFormat(uuid.CleanHyphen)
+
+	// Setup all routes and serve
     r := mux.NewRouter()
     r.HandleFunc("/", hello)
+    r.HandleFunc("/newgame", newGame)
     r.HandleFunc("/game/{gameId}", game)
     r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
     http.Handle("/", r)
     http.ListenAndServe(":8000", nil)
 }
+
+
+// Misc notes:
+/*
+	Games should probably be saved as individual json files, <UUID>.json
+	Those files should have a history of all moves that were made in this game
+
+	Maybe there should also be a copy of the board as it was at the very end?
+	This SHOULD be figure-out-able given just the history of moves, but
+	people making very primitive AIs shouldn't have to roll their own board
+	generator.
+*/
